ba420cb8999247886deabf42571787525743bd40
igor udot <horw7001@gmail.com>
2025-02-20 17:48:29 +0300

    package update

--- setup.py
+++ setup.py
@@ -1,11 +1,1 @@
-from setuptools import setup
-
-setup(
-    name="jupyterlite_embedded_kernel",
-    version="0.1.0",
-    packages=["jupyterlite_embedded_kernel"],
-    install_requires=[
-        "jupyterlab>=4.0.0,<5.0.0",
-    ],
-    package_data={"jupyterlite_embedded_kernel": ["labextension/*"]},
-)
+__import__("setuptools").setup()
