1b1a2d39d0b58d50de8c067bdef3af66c8332f5c
igor.udot <horw7001@gmail.com>
2025-05-27 17:55:24 +0800

    update ci

--- src/constants.ts
+++ src/constants.ts
@@ -23,27 +23,27 @@ export const firmwareOptions: Record<string, FirmwareOption> = {
   },
   'esp32': {
     name: 'ESP32',
-    url: '/files/binaries/ESP32_GENERIC-20250415-v1.25.0.bin',
+    url: './files/binaries/ESP32_GENERIC-20250415-v1.25.0.bin',
     flash_address: 0x1000
   },
   'esp32-s2': {
     name: 'ESP32-S2',
-    url: '/files/binaries/ESP32_GENERIC_S2-20250415-v1.25.0.bin',
+    url: './files/binaries/ESP32_GENERIC_S2-20250415-v1.25.0.bin',
     flash_address: 0x1000
   },
   'esp32-s3': {
     name: 'ESP32-S3',
-    url: '/files/binaries/ESP32_GENERIC_S3-20250415-v1.25.0.bin',
+    url: './files/binaries/ESP32_GENERIC_S3-20250415-v1.25.0.bin',
     flash_address: 0x0
   },
   'esp32-c3': {
     name: 'ESP32-C3',
-    url: '/files/binaries/ESP32_GENERIC_C3-20241129-v1.24.1.bin',
+    url: './files/binaries/ESP32_GENERIC_C3-20241129-v1.24.1.bin',
     flash_address: 0x0
   },
   'esp32-c6': {
     name: 'ESP32-C6',
-    url: '/files/binaries/ESP32_GENERIC_C6-20250415-v1.25.0.bin',
+    url: './files/binaries/ESP32_GENERIC_C6-20250415-v1.25.0.bin',
     flash_address: 0x0
   }
 };
